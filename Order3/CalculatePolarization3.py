80ba0dd073be6cff9260e03a3c4f9cf0e6247f64
ayanc <ayanc@princeton.edu>
2018-05-07 14:06:18 -0400

    Modulation error fixed

--- Order3/CalculatePolarization3.py
+++ Order3/CalculatePolarization3.py
@@ -98,7 +98,7 @@ def nonuniform_frequency_range_3(params, freq_offset=None):
     return np.ascontiguousarray(freq)
 
 
-def get_polarization3(molecule, params):
+def get_polarization3(molecule, params, modulations):
     """
     Return the third order polarization for a specified molecule 
     :param molecule: an instance of ADict describing molecule 
@@ -123,30 +123,21 @@ def get_polarization3(molecule, params):
 
             # reset the polarization because C-code performs "+="
             polarization_mnv[:] = 0.
+            print modulations, m, n, v
+            pol3_total(
+                polarization_mnv, params,
+                modulations[0], modulations[1], modulations[2],
+                energy[n] - energy[v] + 1j * transition[(n, v)].g,
+                energy[m] - energy[v] + 1j * transition[(m, v)].g,
+                energy[v] - energy[0] + 1j * transition[(v, 0)].g,
+                energy[n] - energy[0] + 1j * transition[(n, 0)].g,
+                energy[m] - energy[0] + 1j * transition[(m, 0)].g,
+                energy[m] - energy[n] + 1j * transition[(m, n)].g,
+                energy[n] - energy[m] + 1j * transition[(n, m)].g,
+                energy[m] - energy[0] + 1j * transition[(m, 0)].g,
+                energy[m] - energy[0] + 1j * transition[(m, 0)].g,
 
-            # all_modulations = product(*(3 * [[params.omega_M1, params.omega_M2]]))
-            all_modulations = list(
-                product(*(3 * [[params.omega_M1, params.omega_M2]]))
             )
-            del all_modulations[0:-1]
-            print all_modulations, m, n, v
-
-            for mods in all_modulations:
-                pol3_total(
-                    polarization_mnv, params,
-                    mods[0], mods[1], mods[2],
-                    energy[n] - energy[v] + 1j * transition[(n, v)].g,
-                    energy[m] - energy[v] + 1j * transition[(m, v)].g,
-                    energy[v] - energy[0] + 1j * transition[(v, 0)].g,
-                    energy[n] - energy[0] + 1j * transition[(n, 0)].g,
-                    energy[m] - energy[0] + 1j * transition[(m, 0)].g,
-                    energy[m] - energy[n] + 1j * transition[(m, n)].g,
-                    energy[n] - energy[m] + 1j * transition[(n, m)].g,
-                    energy[m] - energy[0] + 1j * transition[(m, 0)].g,
-                    energy[m] - energy[0] + 1j * transition[(m, 0)].g,
-
-                )
-
             polarization_mnv *= mu_product
             polarization += polarization_mnv
         except KeyError:
@@ -212,13 +203,13 @@ if __name__ == '__main__':
 
     params = ADict(
         N_frequency=2000,
-        comb_size=1,
-        freq_halfwidth=1e1,
+        comb_size=10,
+        freq_halfwidth=0.5e2,
         omega_M1=1.,
         omega_M2=0.,
         gamma=5e-3,
         delta_freq=2.5,
-        width_g=10.
+        width_g=3.
     )
 
     import time
@@ -228,26 +219,50 @@ if __name__ == '__main__':
     params['freq'] = frequency
     print params.freq.size
 
-    pol3 = get_polarization3(molecule, params)
     print time.time() - start
+
     omega = frequency[:, np.newaxis]
     gaussian = np.exp(-(np.arange(-params.comb_size, params.comb_size)) ** 2 / (2.*params.width_g ** 2))[np.newaxis, :]
     comb_omega = (params.delta_freq * np.arange(-params.comb_size, params.comb_size))[np.newaxis, :]
     field1 = (gaussian*(params.gamma / ((omega - params.omega_M1 - comb_omega)**2 + params.gamma**2))).sum(axis=1)
     field2 = (gaussian*(params.gamma / ((omega - params.omega_M2 - comb_omega)**2 + params.gamma**2))).sum(axis=1)
 
-    fig, ax1 = plt.subplots()
-    ax1.plot(frequency, pol3.real, 'k', linewidth=2.)
-    ax1.set_ylabel('$P^{(3)}(\\omega)$', color='k')
-    ax1.tick_params('y', colors='k')
-    ax2 = ax1.twinx()
-    # ax2.plot(frequency, field1, 'b')
-    # ax2.plot(frequency, field2, 'r')
+    all_modulations = list(
+        product(*(3 * [[params.omega_M1, params.omega_M2]]))
+    )
+
+    pol3 = np.zeros((9, params.freq.size), dtype=np.complex)
+    for i, modulations in enumerate(all_modulations):
+        print i, modulations
+        pol3[i] = get_polarization3(molecule, params, modulations)
+
+    pol3_sum = pol3.sum(axis=0)
+    fig, axes = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True)
+    for i in range(3):
+        for j in range(3):
+            if (i != 2) or (j != 2):
+                axes[i, j].set_title('Modulations = {}, {}, {}'.format(*all_modulations[3*i+j]))
+                axes[i, j].plot(frequency, pol3[3*i+j].real, 'k', linewidth=2.)
+                axes[i, j].set_ylabel('$P^{(3)}(\\omega)$', color='k')
+                axes[i, j].tick_params('y', colors='k')
+                ax2 = axes[i, j].twinx()
+                ax2.plot(frequency, field1, 'b', alpha=0.6)
+                ax2.plot(frequency, field2, 'r', alpha=0.6)
+                ax2.set_xlabel("$\\omega_1 + \\omega_2 - \\omega_3 + \\Delta \\omega$ (in GHz)")
+                ax2.set_ylabel('Fields $E(\\omega)$ in $fs^{-1}$', color='b')
+                ax2.tick_params('y', colors='b')
+
+    axes[2, 2].set_title("All modulations")
+    axes[2, 2].plot(frequency, pol3_sum.real, 'k', linewidth=2.)
+    axes[2, 2].set_ylabel('$P^{(3)}(\\omega)$', color='k')
+    axes[2, 2].tick_params('y', colors='k')
+    ax2 = axes[2, 2].twinx()
+    ax2.plot(frequency, field1, 'b', alpha=0.6)
+    ax2.plot(frequency, field2, 'r', alpha=0.6)
     ax2.set_xlabel("$\\omega_1 + \\omega_2 - \\omega_3 + \\Delta \\omega$ (in GHz)")
     ax2.set_ylabel('Fields $E(\\omega)$ in $fs^{-1}$', color='b')
     ax2.tick_params('y', colors='b')
-
-    fig.tight_layout()
+    fig.subplots_adjust(wspace=0.30, hspace=0.25)
     plt.show()
     print time.time() - start
     plt.show()
